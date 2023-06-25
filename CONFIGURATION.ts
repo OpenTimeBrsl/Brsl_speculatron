41484ec5adf031ea9284321bafc7ab455078fd6e
OpenTimeBrsl <136306210+OpenTimeBrsl@users.noreply.github.com>
2023-06-25 20:52:08 +0200

    Update CONFIGURATION.ts

--- CONFIGURATION.ts
+++ CONFIGURATION.ts
@@ -3,6 +3,7 @@ export const AIRTABLE_API_KEY = 'keyyCB522llXkLKKE';
 export const AIRTABLE_DETAILS_FORM = 'https://airtable.com/embed/shryuZ4FQQYP5VUAP';
 
 export const MAPBOX_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9';
+export const MAPBOX_BASE_STYLE = 'mapbox://styles/otcrew/cljb7mpjw003c01o44gwegzv9';
 export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoib3RjcmV3IiwiYSI6ImNsaXQ3aTF4cTA1dmkzaG15cG50N3VsZXcifQ.dW-QCJWKjhmd1PAvJsZrMw';
 
 export const HORIZONTAL_LAYOUT = true; // enable for horizontal layout
